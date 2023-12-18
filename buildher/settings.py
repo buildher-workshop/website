1982d7c0b5d0d7a91becb5fc219b5eaa145408af
Francesco Frassinelli <fraph24@gmail.com>
2023-12-18 22:45:57 +0100

    Add dev and prod profiles

--- buildher/settings.py
+++ buildher/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = "buildher.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
     }
 }
 
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,9 @@ source $(poetry env info --path)/bin/activate
 set -ex
 
 ./manage.py migrate --noinput
+if ! ./manage.py shell -c "from django.conf import settings; exit(settings.DEBUG)"
+then
+    ./manage.py collectstatic --noinput
+fi
 
 exec "$@"
