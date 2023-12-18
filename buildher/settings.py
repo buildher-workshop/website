47d56da5918b1a52a7d57910c9ea9c668d27e4ef
Francesco Frassinelli <fraph24@gmail.com>
2023-12-18 22:20:33 +0100

    Use django-environ

--- buildher/settings.py
+++ buildher/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, "*"),
+)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5p#&bzi!74v4*$(i3lt8welv4i()-16@1qg4mp-)3_cl9^j16c"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = env.parse_value(env("ALLOWED_HOSTS"), list)
+CSRF_TRUSTED_ORIGINS = env.parse_value(env("CSRF_TRUSTED_ORIGINS"), list)
 
 
 # Application definition
